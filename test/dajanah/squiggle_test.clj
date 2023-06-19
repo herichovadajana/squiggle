d2521ff6f4095d68e6394c347530d4adb0f86542
Dajana Herichova <dajana.herichova@gmail.com>
2023-06-19 21:41:29 +0200

    tests

--- test/dajanah/squiggle_test.clj
+++
@@ -1,7 +0,0 @@
-(ns dajanah.squiggle-test
-  (:require [clojure.test :refer :all]
-            [dajanah.squiggle :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
--- test/squiggle/test.clj
+++ test/squiggle/test.clj
@@ -3,9 +3,3 @@
             [squiggle.core :as core]
             [squiggle.open-scad :as os]))
 
-(def create-sphere (core/write (os/sphere {:radius 1})))
-
-(deftest openscad-file
-  (testing "creates and writes into openscad file"
-    (is (= (slurp "CAD/squiggle.scad")
-           "sphere(r=1,$fn=100);"))))

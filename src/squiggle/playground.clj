76bc5486a9bd5b4896bbd7288b85dd4a48495484
Dajana Herichova <dajana.herichova@gmail.com>
2023-06-20 14:56:41 +0200

    playground edit

--- src/squiggle/playground.clj
+++ src/squiggle/playground.clj
@@ -2,10 +2,11 @@
   (:require [squiggle.open-scad :as os]
             [squiggle.core :as core]))
 
-(def resolution 100)
+(def resolution 300)
 
 (defn my-design [radius]
   (os/sphere {:radius radius
               :resolution resolution}))
 
-(core/write (my-design 1))
+(def render (core/write (my-design 1)))
+
